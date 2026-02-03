c4de5a16811fc9ed7d88f5c4b1f9e48f45b78dbf
o-curran <oscurran@gmail.com>
2026-02-03 19:19:36 +0000

    added footer with links

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+    <p className="text-sm text-ring">
+        <a href="https://github.com/vikwritescode/bp-tracker-frontend">GitHub </a>
+         • 
+        <a href="https://ko-fi.com/odhran7861"> Donate</a>
+    </p>
+    )
+}
+
+export default Footer;
